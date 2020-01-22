1a5683fe434da1c0816862807729f473a97dbde7
Tammy Lam <lamtammy1515@gmail.com>
2020-01-22 17:58:08 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,7 @@
 # Use this space to code along with the readme
 
+
+def greeting(phrase)
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +13,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end 
