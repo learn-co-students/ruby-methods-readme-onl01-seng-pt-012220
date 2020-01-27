d19d2ce6bdc9c8b4fa975479f9f3d66b5e34e987
Amanda Contreras <amandaben21@aol.com>
2020-01-27 13:58:17 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,15 @@
-# Use this space to code along with the readme
+def say_hello_world_world_ten_times
+  phrase ="Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+say_hello_world_world_ten_times
